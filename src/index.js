e7007668e7bbb6c467be5855c1d1ebfc976ec698
Ian <DTAwarior@hotmail.com>
2019-12-18 22:19:54 -0500

    Removed boilerplate

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
 import BetrayalCheatSheet from "./components/BetrayalCheatSheet";
 
 ReactDOM.render(<BetrayalCheatSheet />, document.getElementById('root'));
@@ -31,10 +30,4 @@ window.onload=() => {
         }
 
     });
-}
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+};
